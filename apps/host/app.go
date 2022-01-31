53642c2dcffb9d060ced165203327c3891307564
HAOlowkey <39547823+HAOlowkey@users.noreply.github.com>
2022-01-31 16:59:00 +0800

    增加host crud

--- apps/host/app.go
+++ apps/host/app.go
@@ -1,5 +1,138 @@
 package host
 
+import (
+	"crypto/sha1"
+	"encoding/json"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/HAOlowkey/cmdb/apps/resource"
+	"github.com/go-playground/validator/v10"
+	"github.com/imdario/mergo"
+)
+
 const (
 	AppName = "host"
 )
+
+var validate = validator.New()
+
+func (h *Host) GenHash() error {
+	hash := sha1.New()
+
+	b, err := json.Marshal(h.Information)
+	if err != nil {
+		return err
+	}
+	hash.Write(b)
+	h.Base.ResourceHash = fmt.Sprintf("%x", hash.Sum(nil))
+	hash.Reset()
+
+	b, err = json.Marshal(h.Describe)
+	if err != nil {
+		return err
+	}
+	hash.Write(b)
+	h.Base.DescribeHash = fmt.Sprintf("%x", hash.Sum(nil))
+	return nil
+}
+
+func (desc *Describe) KeyPairNameToString() string {
+	return strings.Join(desc.KeyPairName, ",")
+}
+
+func (desc *Describe) SecurityGroupsToString() string {
+	return strings.Join(desc.SecurityGroups, ",")
+}
+
+func (req *DescribeHostRequest) Where() (string, interface{}) {
+	switch req.DescribeBy {
+	case DescribeBy_HOST_ID:
+		return "id = ?", req.Value
+	default:
+		return "", nil
+	}
+}
+
+func NewDefaultHost() *Host {
+	return &Host{
+		Information: &resource.Information{
+			Tags: map[string]string{},
+		},
+		Base:        &resource.Base{},
+		ReleasePlan: &resource.ReleasePlan{},
+		Describe:    &Describe{},
+	}
+}
+
+func (desc *Describe) LoadKeyPairNameString(str string) {
+	desc.KeyPairName = strings.Split(str, ",")
+}
+
+func (desc *Describe) LoadSecurityGroupsString(str string) {
+	desc.SecurityGroups = strings.Split(str, ",")
+}
+
+func NewDescribeHostRequestById(id string) *DescribeHostRequest {
+	return &DescribeHostRequest{
+		DescribeBy: DescribeBy_HOST_ID,
+		Value:      id,
+	}
+}
+
+func NewHostSet() *HostSet {
+	return &HostSet{
+		Items: []*Host{},
+	}
+}
+
+func (set *HostSet) Add(resource *Host) {
+	set.Items = append(set.Items, resource)
+}
+
+func (req *UpdateHostRequest) Validate() error {
+	return validate.Struct(req)
+}
+
+func (host *Host) Put(data *UpdateHostData) {
+	oldResourceHash, oldDescribeHash := host.Base.ResourceHash, host.Base.DescribeHash
+
+	host.Describe = data.Describe
+	host.Information = data.Information
+	host.Information.UpdateAt = time.Now().UnixMilli()
+	host.GenHash()
+
+	if host.Base.DescribeHash != oldDescribeHash {
+		host.Base.DescribeHashChanged = true
+	}
+
+	if host.Base.ResourceHash != oldResourceHash {
+		host.Base.ResourceHashChanged = true
+	}
+}
+
+func (host *Host) Patch(data *UpdateHostData) error {
+	oldResourceHash, oldDescribeHash := host.Base.ResourceHash, host.Base.DescribeHash
+
+	err := mergo.MergeWithOverwrite(host.Describe, data.Describe)
+	if err != nil {
+		return err
+	}
+
+	err = mergo.MergeWithOverwrite(host.Information, data.Information)
+	if err != nil {
+		return err
+	}
+
+	host.Information.UpdateAt = time.Now().UnixMilli()
+	host.GenHash()
+
+	if host.Base.DescribeHash != oldDescribeHash {
+		host.Base.DescribeHashChanged = true
+	}
+
+	if host.Base.ResourceHash != oldResourceHash {
+		host.Base.ResourceHashChanged = true
+	}
+}
--- apps/host/impl/impl.go
+++ apps/host/impl/impl.go
@@ -1,1 +1,49 @@
 package impl
+
+import (
+	"database/sql"
+
+	"github.com/infraboard/mcube/app"
+	"github.com/infraboard/mcube/logger"
+	"github.com/infraboard/mcube/logger/zap"
+	"google.golang.org/grpc"
+
+	"github.com/HAOlowkey/cmdb/apps/host"
+	"github.com/HAOlowkey/cmdb/conf"
+)
+
+var (
+	// Service 服务实例
+	svr = &service{}
+)
+
+type service struct {
+	db   *sql.DB
+	log  logger.Logger
+	host host.ServiceServer
+	host.UnimplementedServiceServer
+}
+
+func (s *service) Config() error {
+	db, err := conf.C().MySQL.GetDB()
+	if err != nil {
+		return err
+	}
+
+	s.log = zap.L().Named(s.Name())
+	s.db = db
+	s.host = app.GetGrpcApp(host.AppName).(host.ServiceServer)
+	return nil
+}
+
+func (s *service) Name() string {
+	return host.AppName
+}
+
+func (s *service) Registry(server *grpc.Server) {
+	host.RegisterServiceServer(server, svr)
+}
+
+func init() {
+	app.RegistryGrpcApp(svr)
+}
